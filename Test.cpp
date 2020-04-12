e9758b24520fac01fea8a8d60a7a10ee157b279b
rinatoz <57804004+rinatoz@users.noreply.github.com>
2020-04-12 22:31:42 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -117,6 +117,7 @@ TEST_CASE("Test 2 remove")
     T.addMother("Moshe", "Calanit");
     T.addFather("Moshe", "Moky");
     T.addFather("Avi", "Moshe1");
+    T.addMother("Avi", "Mazal");
 
     CHECK_THROWS(T.remove("Ori")); // exception
     CHECK((T.find("grandfather") == string("Moshe1") || T.find("grandfather") == string("Moshe")));
@@ -133,4 +134,79 @@ TEST_CASE("Test 2 remove")
     T.addFather("Hanna", "Nissan"); 
     CHECK(T.find("grandfather") == string("Moshe"));
     CHECK(T.find("grandmother") == string("Haya"));
+}
+
+
+TEST_CASE("Test 3") 
+{
+    Tree T("idan");
+    T.addMother("idan", "ravit");
+    T.addFather("idan", "rafi");
+    T.addMother("ravit", "mazal");
+    T.addFather("ravit", "moshe");
+    T.addMother("moshe", "ilanit");
+    T.addFather("moshe", "igal");
+
+    CHECK_THROWS(T.remove("idan")); // exception
+    CHECK(T.find("grandfather") == string("moshe"));
+    CHECK(T.find("father") == string("rafi"));
+    T.remove("rafi");
+    CHECK_THROWS(T.find("father"));
+    CHECK(T.find("grandfather") == string("moshe")); 
+    CHECK(T.find("great-grandmother") == string("ilanit"));
+    CHECK(T.find("great-grandfather") == string("igal"));
+    T.remove("ilanit");
+    CHECK_THROWS(T.find("great-grandmother"));
+}
+TEST_CASE("addFather&addMother&relation") 
+{
+    Tree T("rinat");
+    T.addFather("rinat", "rafi");
+    T.addMother("rinat", "ravit");
+    CHECK(T.relation("rinat") == string("me"));
+    CHECK(T.relation("rafi") == string("father"));
+    CHECK(T.relation("ravit") == string("mother"));
+
+    CHECK_THROWS(T.addFather("rinat", "jfj"));
+    CHECK_THROWS(T.addMother("rinat", "fjdjdf"));
+
+    T.addMother("ravit", "mazal");
+    T.addFather("ravit", "moshe");
+    CHECK(T.relation("mazal") == string("grandmother"));
+    CHECK(T.relation("moshe") == string("grandfather"));
+    CHECK_THROWS(T.addMother("ravit", "fkfkf"));
+    CHECK_THROWS(T.addFather("ravit", "fjfjfj"));
+    CHECK_THROWS(T.addFather("notexist", "jfjjffj")); 
+
+    T.addFather("rafi", "ibo");
+    T.addMother("rafi", "rina");
+    CHECK(T.relation("ibo") == string("grandfather"));
+    CHECK(T.relation("rina") == string("grandmother"));
+    CHECK_THROWS(T.addFather("rafi", "mcmc"));
+    CHECK_THROWS(T.addMother("rafi", "cjcjcj"));
+
+    T.addFather("ibo", "ibof");
+    T.addMother("ibo", "ibom");
+    CHECK(T.relation("ibof") == string("great-grandfather"));
+    CHECK(T.relation("ibom") == string("great-grandmother"));
+    CHECK_THROWS(T.addFather("ibo", "mcmc"));
+    CHECK_THROWS(T.addMother("ibo", "cjcjcj"));
+
+    T.addFather("ibom", "ibomf");
+    T.addMother("ibom", "ibomm");
+    CHECK(T.relation("ibomf") == string("great-great-grandfather"));
+    CHECK(T.relation("ibomm") == string("great-great-grandmother"));
+    CHECK_THROWS(T.addFather("ibom", "mcmc"));
+    CHECK_THROWS(T.addMother("ibom", "cjcjcj"));
+
+    CHECK(T.relation("rin") == string("unrelated"));
+    CHECK(T.relation("ben") == string("unrelated"));
+    CHECK(T.relation("daniel") == string("unrelated"));
+    CHECK(T.relation("roy") == string("unrelated"));
+    CHECK(T.relation("yossi") == string("unrelated"));
+    CHECK(T.relation("0") == string("unrelated"));
+    CHECK(T.relation("t") == string("unrelated"));
+    CHECK(T.relation("amit") == string("unrelated"));
+    CHECK(T.relation("nati") == string("unrelated"));
+    CHECK(T.relation("yossef") == string("unrelated"));
 }
